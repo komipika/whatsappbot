fd9a359b2a56363bc58fb2cc9de14e389152be14
komipika <44190957+komipika@users.noreply.github.com>
2021-10-14 22:08:02 +0300

    Added more functionality
    You can now ask for help and all veg options

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ TIES4560 Course Week 6 demo showcasing serverless based cloud function implement
 
 To be deployed to Google cloud platform.
 
+Instructions:
+-> Create function (check "Allow unauthenticated invocations" to make webhook public)
+
+-> Set the "Runtime" to "Python 3.7" and the “Entry point” to ‘whatsapp_webhook’.
+
+-> Replace contents of the *main.py* file on the left with those from the main.py
+-> Do the same for the requirements.txt file.
+
+-> Click "DEPLOY" -> If succesful, you will see a green tick next to your function name
--- bot/main.py
+++ bot/main.py
@@ -23,41 +23,107 @@ def whatsapp_webhook(request):
   twilio_response = MessagingResponse()
   msg = twilio_response.message()
 
+  
   if message in restaurants.keys():
-    resp = requests.get(restaurants[message])
-    if not (200 <= resp.status_code <= 299):
-        logger.error(
-            f"Failed to retrieve data for the following restaurant - {message}. Here is a more verbose reason {resp.reason}"
-        )
-        msg.body(
-            "Sorry we could not process your request. Please try again or check a different restaurant. You can type \"help\" for available commands."
-        )
-    else:
-        name = resp.json()["RestaurantName"]
-        time = resp.json()["MenusForDays"][0]["LunchTime"]
-        menus = resp.json()["MenusForDays"][0]["SetMenus"]
-        logger.info(menus)
-        ret = f"Todays lunch menu for {name}: \n "
-        ret += (f"Lunchtime at {time}, \n")
-        for food in menus:
-          logger.info(food)
-          tempStr = ""
-          for comp in food["Components"]:
-            tempStr += f"{comp} "
-          tempName = food["Name"]
-          ret += (f"{tempName}: {tempStr}, \n")
-        msg.body(
-          ret
-        )
-  else:
-    logger.error(
-      f"Failed to retrieve data for the following restaurant - {message}. No match in Dictionary."
-    )
+    resp = getTodaysMenu(message)
     msg.body(
-      "Sorry we could not process your request. Please try again or check a different restaurant"
+      resp
     )
+    return str(twilio_response)
+  if message == "kasvismenu":
+    resp = getVegMenus()
+    msg.body(
+      resp
+    )
+    return str(twilio_response)
+
+  if message == "help":
+    resp = getSomeHelp()
+    msg.body(
+      resp
+    )
+    return str(twilio_response)
+  
+  msg.body(
+    "Sorry we could not process your request. Please try again or check a different restaurant. You can type \"help\" for available commands."
+  )
     
   return str(twilio_response)
+
+
+def getTodaysMenu(message):
+  resp = requests.get(restaurants[message])
+  if not (200 <= resp.status_code <= 299):
+    logger.error(
+        f"Failed to retrieve data for the following restaurant - {message}. Here is a more verbose reason {resp.reason}"
+    )
+    return "Sorry we could not process your request. Please try again or check a different restaurant. You can type \"help\" for available commands."
+  else:
+    name = resp.json()["RestaurantName"]
+    if not resp.json()["MenusForDays"]:
+      return f"{name} is not offering anything today."
+    time = resp.json()["MenusForDays"][0]["LunchTime"]
+    menus = resp.json()["MenusForDays"][0]["SetMenus"]
+    ret = f"Todays lunch menu for {name}: \n "
+    ret += (f"Lunchtime at {time}, \n")
+    for food in menus:
+      tempStr = ""
+      for comp in food["Components"]:
+        tempStr += f"{comp} "
+      tempName = food["Name"]
+      ret += (f"{tempName}: {tempStr}, \n")    
+    ret = ret.strip()
+    ret = ret[:-1]
+    return ret
+
+  
+def getVegMenus():
+  logger.info("getting vege menus")
+  ret = "Todays vegetable options are: \n"
+  for key in restaurants:
+    ret += getVeg(restaurants[key])
+  ret = ret.strip()
+  ret = ret[:-1]
+  return ret
+
+def getVeg(url):
+  resp = requests.get(url)
+  if not (200 <= resp.status_code <= 299):
+    logger.error(
+        f"Failed to retrieve data for the restaurant. Here is a more verbose reason {resp.reason}"
+    )
+    return ""
+  else:
+    if not resp.json()["MenusForDays"]:
+      return ""
+    name = resp.json()["RestaurantName"]
+    logger.info(name)
+    menus = resp.json()["MenusForDays"][0]["SetMenus"]
+    ret = f"{name}:"
+    for food in menus:
+      tempStr = ""
+      tempName = food["Name"]
+      if tempName not in vegAlias:
+        continue #go next if not a veg option
+      for comp in food["Components"]:
+        tempStr += f"{comp} "
+      ret += (f" {tempStr}, \n")   
+
+    if ret == f"{name}:":
+      return "" #return empty string if no vegan options
+
+    return ret
+
+
+def getSomeHelp():
+  ret = "Type \"Kasvismenu\" for all vegan lunch options for semma restaurants today. \n"
+  ret += "You can also type restaurants name to get its full lunch menu. \n"
+  ret += "Available restaurants: \n"
+  for key in restaurants:
+    ret += f"{key}, \n"
+  ret = ret.strip()
+  ret = ret[:-1] #trim last "," off
+  return ret
 
 #Dict of semmas' json menus in finnish
 restaurants = {
@@ -76,19 +142,13 @@ restaurants = {
   "taide":"https://www.foodandco.fi/modules/json/json/Index?costNumber=0301&language=fi"
 }
 
-#Dict of semmas' json menus in english
-restaurantsEn = {
-  "piato": "https://www.semma.fi/modules/json/json/Index?costNumber=1408&language=en",
-  "maija": "https://www.semma.fi/modules/json/json/Index?costNumber=1402&language=en",
-  "lozzi":"https://www.semma.fi/modules/json/json/Index?costNumber=1401&language=en",
-  "belvedere":"https://www.semma.fi/modules/json/json/Index?costNumber=1404&language=en",
-  "syke":"https://www.semma.fi/modules/json/json/Index?costNumber=1405&language=en",
-  "tilia":"https://www.semma.fi/modules/json/json/Index?costNumber=1413&language=en",
-  "uno":"https://www.semma.fi/modules/json/json/Index?costNumber=1414&language=en",
-  "ylisto":"https://www.semma.fi/modules/json/json/Index?costNumber=1403&language=en",
-  "kvarkki":"https://www.semma.fi/modules/json/json/Index?costNumber=140301&language=en",
-  "rentukka":"https://www.semma.fi/modules/json/json/Index?costNumber=1416&language=en",
-  "novelli":"https://www.semma.fi/modules/json/json/Index?costNumber=1409&language=en",
-  "fiilu":"https://www.foodandco.fi/modules/json/json/Index?costNumber=3364&language=en",
-  "taide":"https://www.foodandco.fi/modules/json/json/Index?costNumber=0301&language=en"
-}
+#different ways veg option are spelt in JSON
+vegAlias = (
+  "KASVISLOUNAS",
+  "Kasvislounas",
+  "kasvislounas",
+  "KASVISKEITTO",
+  "Kasviskeitto",
+  "kasviskeitto",
+  "Kasviskietto"
+)
